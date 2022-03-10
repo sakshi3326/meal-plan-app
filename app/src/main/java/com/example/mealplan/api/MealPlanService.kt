a6b82dc0d84bca5a43b56bccedd29f3c5d4744a0
Ander <ander.the.enderwolf@gmail.com>
2022-03-10 11:53:44 -0800

    Created API service and Data Classes
    Not complete, Somehow need to get the service to interact with datatypes. Don't understand Hess's method for Moshi.

---
+++ app/src/main/java/com/example/mealplan/api/MealPlanService.kt
@@ -0,0 +1,40 @@
+package com.example.mealplan.api
+
+import com.example.mealplan.data.IngredientsList
+import com.example.mealplan.data.MealPlanJsonAdapter
+import com.example.mealplan.ui.IngredientsAdapter
+import com.example.mealplan.ui.MealAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MealPlanService {
+	//https://api.nal.usda.gov/fdc/v1/foods/search?api_key=kkYBL2fNKTOGlVPzlcndfmcXQ0KhGgL0Y3McXwgn&query=
+	@GET("search")
+	suspend fun loadFiveDayForecast(
+		@Query("api_key") apiKey: String = "kkYBL2fNKTOGlVPzlcndfmcXQ0KhGgL0Y3McXwgn",
+		@Query("query") city: String?
+	) : IngredientsList
+
+	companion object {
+		private const val BASE_URL = "https://api.nal.usda.gov/fdc/v1/foods/"
+
+		/**
+		 * This method is used to create an instance of the [MealPlanService] interface.
+		 */
+		fun create() : MealPlanService {
+			val moshi = Moshi.Builder()
+				.add(MealPlanJsonAdapter()) //TODO(Idk how or why Hess did this, Need to figure it out)
+				.addLast(KotlinJsonAdapterFactory())
+				.build()
+			return Retrofit.Builder()
+				.baseUrl(BASE_URL)
+				.addConverterFactory(MoshiConverterFactory.create(moshi))
+				.build()
+				.create(MealPlanService::class.java)
+		}
+	}
+}
